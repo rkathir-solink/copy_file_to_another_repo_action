038865d05357fbc406a64dc7c203b646d0b9969e
rkathir-solink <rkathir@solinkcorp.com>
2021-05-27 11:20:10 -0400

    install ssh-client

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ echo "Cloning destination git repository"
 #git config --global user.email "$INPUT_USER_EMAIL"
 #git config --global user.name "$INPUT_USER_NAME"
 #git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+apt-get install ssh-client
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "git@github.com:rkathir-solink/solink-parser-config.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
